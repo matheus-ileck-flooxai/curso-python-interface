4989517fb96e8ee676f476335da30a1bfb551980
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-07-01 09:48:05 -0300

    feat: add actions with signals

--- pyside6/aula3.py
+++ pyside6/aula3.py
@@ -1,15 +1,7 @@
-# QMainWindow e centralWidget
-# -> QApplication (app)
-#   -> QMainWindow (window->setCentralWidget)
-#       -> CentralWidget (central_widget)
-#           -> Layout (layout)
-#               -> Widget 1 (botao1)
-#               -> Widget 2 (botao2)
-#               -> Widget 3 (botao3)
-#   -> show
-# -> exec
+# O básico sobre Signal e Slots (eventos e documentação)
 import sys
 
+from PySide6.QtCore import Slot
 from PySide6.QtWidgets import (QApplication, QGridLayout, QMainWindow,
                                QPushButton, QWidget)
 
@@ -36,8 +28,23 @@ layout.addWidget(botao2, 1, 2, 1, 1)
 layout.addWidget(botao3, 3, 1, 1, 2)
 
 
+@Slot()
 def slot_example(status_bar):
-    status_bar.showMessage(f'1 + 1 é {1 + 1}')
+    def inner():
+        status_bar.showMessage('O meu slot foi executado')
+    return inner
+
+
+@Slot()
+def outro_slot(checked):
+    print('Está marcado?', checked)
+
+
+@Slot()
+def terceiro_slot(action):
+    def inner():
+        outro_slot(action.isChecked())
+    return inner
 
 
 # statusBar
@@ -48,10 +55,14 @@ status_bar.showMessage('Mostrar mensagem na barra')
 menu = window.menuBar()
 primeiro_menu = menu.addMenu('Primeiro menu')
 primeira_acao = primeiro_menu.addAction('Primeira ação')
-primeira_acao.triggered.connect(  
-    lambda: slot_example(status_bar)
-)
+primeira_acao.triggered.connect(slot_example(status_bar))  
 
+segunda_action = primeiro_menu.addAction('Segunda ação')
+segunda_action.setCheckable(True)
+segunda_action.toggled.connect(outro_slot)  
+segunda_action.hovered.connect(terceiro_slot(segunda_action)) 
+
+botao1.clicked.connect(terceiro_slot(segunda_action))  
 
 window.show()
 app.exec()  # O loop da aplicação
