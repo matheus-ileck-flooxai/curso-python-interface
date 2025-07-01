a8ed8ccbc64a728ac089c4fc1bd463800df24ec8
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-07-01 12:43:09 -0300

    feat: add custom class Button

--- pyside6/calculadora/main.py
+++ pyside6/calculadora/main.py
@@ -1,6 +1,7 @@
 import sys
 from PySide6.QtWidgets import QApplication
 from info import Info, QLabel
+from buttons import Button
 from display import Display
 from main_window import MainWindow
 from PySide6.QtGui import QIcon
@@ -21,9 +22,14 @@ if __name__ == '__main__':
     info = Info('2.0 ^ 10.0 = 1024')
     window.addToVLayout(info)
 
+
     # Display
     display = Display()
     window.addToVLayout(display)
+
+    # Botao
+    button = Button('Texto do botão')
+    window.addToVLayout(button)
 
     # Define um tamanho Fixo da janela
     window.adjustFixedSize()
